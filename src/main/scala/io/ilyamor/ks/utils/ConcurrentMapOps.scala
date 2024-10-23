352b60ce83ca77532630ec1051f2df9e1ab4d2f5
Mikhail <grinfeld@gmail.com>
2024-10-23 10:42:06 +0300

    add support for flush frequency (#3)

---
+++ src/main/scala/io/ilyamor/ks/utils/ConcurrentMapOps.scala
@@ -0,0 +1,25 @@
+package io.ilyamor.ks.utils
+
+import org.apache.kafka.streams.state.internals.StateStoreToS3.SnapshotStoreListeners.SnapshotStoreListener.FlushingState
+import org.apache.kafka.streams.state.internals.StateStoreToS3.SnapshotStoreListeners.TppStore
+
+import java.util.concurrent.ConcurrentMap
+
+object ConcurrentMapOps {
+  implicit class ConcurrentMapOps(concurrentMap: ConcurrentMap[TppStore, FlushingState]) {
+    def getOps(key: TppStore): Option[FlushingState] = {
+      val value = concurrentMap.get(key)
+      if (value == null) None else Option(value)
+    }
+
+    def availableForWork(key: TppStore, now: Long, frequencyMs: Long): Boolean = {
+      getOps(key) match {
+        case None => true
+        case Some(state) if state.inWork || now - state.lastFlush <= frequencyMs => false
+        case _ => true
+      }
+    }
+  }
+
+
+}
